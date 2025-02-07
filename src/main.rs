f0176472ee3acab985b55a89b04d3c65f923fcf5
dangow3 <daniel.d@protokol.com>
2025-02-07 14:08:13 +0200

    Added file logging.

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,28 @@ use copy_trade_telegram::{
     tg_copy::downloader::async_main,
 };
 use dotenv::dotenv;
-use std::sync::Arc;
-use tracing_subscriber::filter::LevelFilter;
-use tracing_subscriber::EnvFilter;
+use std::{io, sync::Arc};
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
 
-    // Configure logging
+    let file_appender = RollingFileAppender::new(Rotation::DAILY, "logs", "trade-bot.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
     let filter = EnvFilter::builder()
         .with_default_directive(LevelFilter::INFO.into())
         .parse_lossy("copy_trade_telegram=info,grammers_session=warn");
 
-    tracing_subscriber::fmt().with_env_filter(filter).init();
+    tracing_subscriber::registry()
+        .with(fmt::Layer::new().with_writer(io::stdout))
+        .with(fmt::Layer::new().with_writer(non_blocking))
+        .with(filter)
+        .init();
 
     let signer = LocalSolanaSigner::new(env("SOLANA_PRIVATE_KEY"));
-
     SignerContext::with_signer(Arc::new(signer), async { async_main().await }).await?;
 
     Ok(())
